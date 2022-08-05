7748184fb5b561b4527796f43ca438146d737186
gonzalo <gonzalo@pop-os.localdomain>
2022-08-05 14:54:35 -0300

    excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let pronoun = ["El", "Un"];
+  let subject = [
+    "cerdo con rabia",
+    "taxista ebrio",
+    "gracioso",
+    "pajaro furioso",
+    "robot poseido"
+  ];
+  let action = ["tiro mi", "se comió mi", "escupió mi", "robó mi"];
+  let posession = ["trabajo práctico", "dedo del pie", "auto", "zapato"];
+  let where = ["en mi casa", "en la calle", "camino hacia acá"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let posessionIndex = Math.floor(Math.random() * posession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    posession[posessionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
